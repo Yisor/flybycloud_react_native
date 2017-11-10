16ee3750665de94755b5be6bfe616217ab68d460
liushenglin <liushenglin@flybycloud.com>
2017-11-10 18:39:26 +0800

    login action改为es6写法

--- src/actions/login.js
+++ src/actions/login.js
@@ -2,9 +2,13 @@
 import * as TYPES from './types';
 
 //  login
-export function login(opt) {
-  return {
-    'type': TYPES.LOGGED_IN,
-    'user': opt,
-  }
-}
+// export function login(opt) {
+//   return {
+//     'type': TYPES.LOGGED_IN,
+//     'user': opt,
+//   }
+// }
+
+// es6写法
+export const login = data => ({ 'type': TYPES.LOGGED_IN, 'user': data })
+
